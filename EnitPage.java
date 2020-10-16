96fbcb9595f431d7a589903161b156027375d4a9
Kirill <72563635+KirillFroloff@users.noreply.github.com>
2020-10-16 21:12:40 +0300

    Update EnitPage.java

--- EnitPage.java
+++ EnitPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class EnitPage extends Sbase {
+   
     @Epic("Селениум")
     @Feature("Гугле тетс")
     @Story("Проверяем картинки")
